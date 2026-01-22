cbe4389b71d63939ae8967c4d60e1f390089d17e
Cersat <serba8a9a@gmail.com>
2026-01-22 11:08:37 +0400

    v110 update

---
+++ src/HexagonPins.h
@@ -0,0 +1,22 @@
+#ifndef HEXAGONPINS_H
+#define HEXAGONPINS_H
+
+#include <arduino.h>
+
+typedef const int pin;
+
+extern pin twoColorLed_p1 = 3;
+extern pin twoColorLed_p2 = 6;
+extern pin ledDisplay_data = 7;
+extern pin ledDisplay_clock = 8;
+extern pin sensors_b1 = A2;
+extern pin sensors_b2 = A1;
+extern pin sensors_b3 = A0;
+extern pin sensors_pot = A3;
+extern pin ipsDisplay_rst = 4;
+extern pin ipsDisplay_dc = 10;
+
+
+
+
+#endif
